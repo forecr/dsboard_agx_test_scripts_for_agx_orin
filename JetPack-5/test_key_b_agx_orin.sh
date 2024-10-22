42b3e32179d7d67ce94a8bee2d084e6591076d8b
mdeniz0 <mehmet@forecr.io>
2024-10-22 13:53:40 +0300

    M.2 key-B tests updated. 2*GPIOs DISABLE1# & DISABLE2# pins added. PWRON GPIOs renamed

--- JetPack-5/test_key_b_agx_orin.sh
+++ JetPack-5/test_key_b_agx_orin.sh
@@ -1,13 +1,19 @@
+sudo echo 452 > /sys/class/gpio/export
+sudo echo 444 > /sys/class/gpio/export
 sudo echo 350 > /sys/class/gpio/export
 sudo echo 446 > /sys/class/gpio/export
 sudo echo 349 > /sys/class/gpio/export
 
+sudo echo high > /sys/class/gpio/PQ.04/direction
+sudo echo high > /sys/class/gpio/PP.04/direction
 sudo echo high > /sys/class/gpio/PA.02/direction
 sudo echo low > /sys/class/gpio/PP.06/direction
 sudo echo high > /sys/class/gpio/PA.01/direction
 
 trap interrupt_func INT
 interrupt_func() {
+	sudo echo 452 > /sys/class/gpio/unexport
+	sudo echo 444 > /sys/class/gpio/unexport
 	sudo echo 350 > /sys/class/gpio/unexport
 	sudo echo 446 > /sys/class/gpio/unexport
 	sudo echo 349 > /sys/class/gpio/unexport
--- JetPack-6/test_key_b_agx_orin.sh
+++ JetPack-6/test_key_b_agx_orin.sh
@@ -7,18 +7,24 @@ fi
 #gpiochip0 - tegra234-gpio
 #gpiochip1 - tegra234-gpio-aon
 
+gpioset --mode=signal `gpiofind "PQ.04"`=1 &
+PID_M2B_ENABLE1=$!
+gpioset --mode=signal `gpiofind "PP.04"`=1 &
+PID_M2B_ENABLE2=$!
 gpioset --mode=signal `gpiofind "PA.02"`=1 &
-PID_M2B_PWRON=$!
+PID_M2B_FULLCARD_PWRON=$!
 gpioset --mode=signal `gpiofind "PP.06"`=0 &
 PID_M2B_RESET=$!
 gpioset --mode=signal `gpiofind "PA.01"`=1 &
-PID_M2B_PWROFF=$!
+PID_M2B_PWRON=$!
 
 trap interrupt_func INT
 interrupt_func() {
-	kill $PID_M2B_PWRON
+	kill $PID_M2B_ENABLE1
+	kill $PID_M2B_ENABLE2
+	kill $PID_M2B_FULLCARD_PWRON
 	kill $PID_M2B_RESET
-	kill $PID_M2B_PWROFF
+	kill $PID_M2B_PWRON
 }
 
 watch -n 0.1 lsusb
